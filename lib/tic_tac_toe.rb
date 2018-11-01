a89dc35936ded0f36f69093332d8721f4a01cd09
Talisa Erazo <talisaerazo@gmail.com>
2018-11-01 04:43:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,15 @@
 class TicTacToe
 
-  def initialize
-
+  def initialize(board)
+    @board = display_board
   end
+
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+
 end
