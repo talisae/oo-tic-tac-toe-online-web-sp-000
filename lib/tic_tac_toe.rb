c7aa10c68dc0fc570241750a2fa3b977ccd06622
Talisa Erazo <talisaerazo@gmail.com>
2018-11-01 05:35:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,8 +58,8 @@ class TicTacToe
   def won?
    WIN_COMBINATIONS.any? do |combo|
     if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
-    return combo
-    end
+      return combo
+      end
      end
   end
 end
